9c02b59d20c0cf9994b5887993dd603c5a872eeb
funzter <cavehive@protonmail.com>
2023-04-16 13:51:03 +0200

    cleaned up

--- src/main/java/de/timesnake/library/chat/ExTextColor.java
+++ src/main/java/de/timesnake/library/chat/ExTextColor.java
@@ -11,9 +11,22 @@ import net.kyori.adventure.util.Index;
 
 public class ExTextColor implements TextColor {
 
+    public static final ExTextColor PUBLIC = new ExTextColor('p', 'f',
+            "public", NamedTextColor.WHITE);
+    public static final ExTextColor PERSONAL = new ExTextColor('s', 'e',
+            "personal", NamedTextColor.YELLOW);
+    public static final ExTextColor WARNING = new ExTextColor('w', 'c',
+            "warning", NamedTextColor.RED);
+    public static final ExTextColor VALUE = new ExTextColor('v', '7',
+            "value", NamedTextColor.GRAY);
+    public static final ExTextColor QUICK_INFO = new ExTextColor('q', '8',
+            "quick_info", NamedTextColor.DARK_GRAY);
+    public static final ExTextColor HIGHLIGHTED_TEXT = new ExTextColor('h', '6',
+            "highlighted_text", NamedTextColor.GOLD);
+    public static final ExTextColor HIGHLIGHTED_VALUE = new ExTextColor('u', '9',
+            "highlighted_value", NamedTextColor.BLUE);
+
     public static final ExTextColor BLACK = new ExTextColor('0', NamedTextColor.BLACK);
-    public static final ExTextColor QUICK_INFO = new ExTextColor('q', "quick_info",
-            NamedTextColor.DARK_GRAY);
     public static final ExTextColor DARK_BLUE = new ExTextColor('1', NamedTextColor.DARK_BLUE);
     public static final ExTextColor DARK_GREEN = new ExTextColor('2', NamedTextColor.DARK_GREEN);
     public static final ExTextColor DARK_AQUA = new ExTextColor('3', NamedTextColor.DARK_AQUA);
@@ -21,27 +34,26 @@ public class ExTextColor implements TextColor {
     public static final ExTextColor DARK_PURPLE = new ExTextColor('5', NamedTextColor.DARK_PURPLE);
     public static final ExTextColor GOLD = new ExTextColor('6', NamedTextColor.GOLD);
     public static final ExTextColor GRAY = new ExTextColor('7', NamedTextColor.GRAY);
-    public static final ExTextColor VALUE = new ExTextColor('v', "value", NamedTextColor.GRAY);
     public static final ExTextColor DARK_GRAY = new ExTextColor('8', NamedTextColor.DARK_GRAY);
     public static final ExTextColor BLUE = new ExTextColor('9', NamedTextColor.BLUE);
     public static final ExTextColor GREEN = new ExTextColor('a', NamedTextColor.GREEN);
     public static final ExTextColor AQUA = new ExTextColor('b', NamedTextColor.AQUA);
     public static final ExTextColor RED = new ExTextColor('c', NamedTextColor.RED);
-    public static final ExTextColor WARNING = new ExTextColor('w', "warning", NamedTextColor.RED);
     public static final ExTextColor LIGHT_PURPLE = new ExTextColor('d',
             NamedTextColor.LIGHT_PURPLE);
     public static final ExTextColor YELLOW = new ExTextColor('e', NamedTextColor.YELLOW);
-    public static final ExTextColor PERSONAL = new ExTextColor('s', "personal",
-            NamedTextColor.YELLOW);
     public static final ExTextColor WHITE = new ExTextColor('f', NamedTextColor.WHITE);
-    public static final ExTextColor PUBLIC = new ExTextColor('p', "public", NamedTextColor.WHITE);
-    public static final ExTextColor RESET = new ExTextColor('r', "reset", NamedTextColor.WHITE);
+    public static final ExTextColor RESET = new ExTextColor('r', 'r',
+            "reset", NamedTextColor.WHITE);
 
     public static final List<ExTextColor> VALUES = List.of(BLACK, QUICK_INFO, DARK_BLUE, DARK_GREEN,
-            DARK_AQUA, DARK_RED,
+            DARK_AQUA, DARK_RED, HIGHLIGHTED_TEXT, HIGHLIGHTED_VALUE,
             DARK_PURPLE, GOLD, GRAY, VALUE, DARK_GRAY, BLUE, GREEN, AQUA, RED, WARNING,
             LIGHT_PURPLE, YELLOW, PERSONAL,
             WHITE, PUBLIC, RESET);
+
+    public static final List<ExTextColor> CUSTOM_VALUES = List.of(PUBLIC, PERSONAL, WARNING,
+            VALUE, QUICK_INFO, HIGHLIGHTED_VALUE, HIGHLIGHTED_TEXT);
 
     public static final Index<String, ExTextColor> NAMES = Index.create(ExTextColor::getName,
             VALUES);
@@ -49,17 +61,20 @@ public class ExTextColor implements TextColor {
             VALUES);
 
     private final char token;
+    private final char legacyToken;
     private final NamedTextColor color;
     private final String name;
 
     public ExTextColor(char token, NamedTextColor color) {
         this.token = token;
+        this.legacyToken = token;
         this.color = color;
         this.name = color.toString();
     }
 
-    public ExTextColor(char token, String name, NamedTextColor color) {
+    public ExTextColor(char token, char legacyToken, String name, NamedTextColor color) {
         this.token = token;
+        this.legacyToken = legacyToken;
         this.color = color;
         this.name = name;
     }
@@ -68,8 +83,8 @@ public class ExTextColor implements TextColor {
         return token;
     }
 
-    public String getLegacyColor() {
-        return "§" + this.getToken();
+    public String getLegacyToken() {
+        return "§" + this.legacyToken;
     }
 
     public String getName() {
