30bb3f2cd2951d36470f336f8bbf86168f600f38
funzter <funzter@mail.timesnake.de>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/library/chat/TimeDownParser.java
+++ src/main/java/de/timesnake/library/chat/TimeDownParser.java
@@ -11,77 +11,77 @@ import net.kyori.adventure.text.format.TextDecoration;
 
 public class TimeDownParser {
 
-    public Component parse2Component(String text) {
-        return parse2Component(Component.empty(), text, ExTextColor.WHITE, new LinkedList<>());
+  public Component parse2Component(String text) {
+    return parse2Component(Component.empty(), text, ExTextColor.WHITE, new LinkedList<>());
+  }
+
+  protected Component parse2Component(Component component, String text, ExTextColor currentColor,
+      List<TextDecoration> decorations) {
+
+    int tokenIndex = getNextTokenKeyIndex(text, 0);
+
+    if (tokenIndex == -1) {
+      return component.append(Component.text(text.replace("\\", ""), currentColor)
+          .decorate(decorations.toArray(TextDecoration[]::new)));
     }
 
-    protected Component parse2Component(Component component, String text, ExTextColor currentColor,
-            List<TextDecoration> decorations) {
-
-        int tokenIndex = getNextTokenKeyIndex(text, 0);
-
-        if (tokenIndex == -1) {
-            return component.append(Component.text(text.replace("\\", ""), currentColor)
-                    .decorate(decorations.toArray(TextDecoration[]::new)));
-        }
-
-        if (tokenIndex > 0) {
-            component = component.append(
-                    Component.text(text.substring(0, tokenIndex).replace("\\", ""), currentColor)
-                            .decorate(decorations.toArray(TextDecoration[]::new)));
-        }
-
-        ExTextColor nextColor;
-
-        do {
-            tokenIndex++;
-            char token = text.charAt(tokenIndex);
-
-            nextColor = ExTextColor.TOKENS.value(token);
-
-            if (nextColor == null) {
-                ExDecoration decoration = ExDecoration.TOKENS.value(token);
-
-                if (decoration != null) {
-                    decorations.add(decoration.getDecoration());
-                } else {
-                    throw new TimeDownParseException(
-                            "Invalid format-option '" + token + "' at index " + tokenIndex);
-                }
-            } else {
-                decorations.clear();
-            }
-        } while (text.charAt(++tokenIndex) == '§');
-
-        if (nextColor == null) {
-            nextColor = ExTextColor.WHITE;
-        }
-
-        String nextText = text.substring(tokenIndex);
-
-        return parse2Component(component, nextText, nextColor, decorations);
+    if (tokenIndex > 0) {
+      component = component.append(
+          Component.text(text.substring(0, tokenIndex).replace("\\", ""), currentColor)
+              .decorate(decorations.toArray(TextDecoration[]::new)));
     }
 
-    public String parse2Legacy(String text) {
-        for (ExTextColor c : ExTextColor.CUSTOM_VALUES) {
-            text = text.replace("§" + c.getToken(), "§" + c.getLegacyToken());
+    ExTextColor nextColor;
+
+    do {
+      tokenIndex++;
+      char token = text.charAt(tokenIndex);
+
+      nextColor = ExTextColor.TOKENS.value(token);
+
+      if (nextColor == null) {
+        ExDecoration decoration = ExDecoration.TOKENS.value(token);
+
+        if (decoration != null) {
+          decorations.add(decoration.getDecoration());
+        } else {
+          throw new TimeDownParseException(
+              "Invalid format-option '" + token + "' at index " + tokenIndex);
         }
-        return text;
+      } else {
+        decorations.clear();
+      }
+    } while (text.charAt(++tokenIndex) == '§');
+
+    if (nextColor == null) {
+      nextColor = ExTextColor.WHITE;
     }
 
+    String nextText = text.substring(tokenIndex);
 
-    protected int getNextTokenKeyIndex(String text, int startIndex) {
-        int tokenKeyIndex = startIndex - 1;
+    return parse2Component(component, nextText, nextColor, decorations);
+  }
 
-        do {
-            tokenKeyIndex = text.indexOf('§', tokenKeyIndex + 1);
-            if (tokenKeyIndex >= text.length() + 1) {
-                tokenKeyIndex = -1;
-                break;
-            }
-        } while (tokenKeyIndex > 0 && text.charAt(tokenKeyIndex - 1) == '\\');
-
-        return tokenKeyIndex;
+  public String parse2Legacy(String text) {
+    for (ExTextColor c : ExTextColor.CUSTOM_VALUES) {
+      text = text.replace("§" + c.getToken(), "§" + c.getLegacyToken());
     }
+    return text;
+  }
+
+
+  protected int getNextTokenKeyIndex(String text, int startIndex) {
+    int tokenKeyIndex = startIndex - 1;
+
+    do {
+      tokenKeyIndex = text.indexOf('§', tokenKeyIndex + 1);
+      if (tokenKeyIndex >= text.length() + 1) {
+        tokenKeyIndex = -1;
+        break;
+      }
+    } while (tokenKeyIndex > 0 && text.charAt(tokenKeyIndex - 1) == '\\');
+
+    return tokenKeyIndex;
+  }
 
 }
--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -12,23 +12,23 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class ParseTest {
 
-    public static Stream<Arguments> parseStrings() {
-        return Stream.of(
-                Arguments.of("test", "§ftest"),
-                Arguments.of("§6test", "§6test"),
-                Arguments.of("\\§6test", "§f§6test"),
-                Arguments.of("§ptest", "§ftest"),
-                Arguments.of("§stest", "§etest"),
-                Arguments.of("§vtest", "§7test"),
-                Arguments.of("§6te §cst", "§6te §cst")
-        );
-    }
+  public static Stream<Arguments> parseStrings() {
+    return Stream.of(
+        Arguments.of("test", "§ftest"),
+        Arguments.of("§6test", "§6test"),
+        Arguments.of("\\§6test", "§f§6test"),
+        Arguments.of("§ptest", "§ftest"),
+        Arguments.of("§stest", "§etest"),
+        Arguments.of("§vtest", "§7test"),
+        Arguments.of("§6te §cst", "§6te §cst")
+    );
+  }
 
-    @ParameterizedTest
-    @MethodSource("parseStrings")
-    public void parse(String text, String expected) {
-        Assertions.assertEquals(expected, LegacyComponentSerializer.legacySection()
-                .serialize(new TimeDownParser().parse2Component(text)));
-    }
+  @ParameterizedTest
+  @MethodSource("parseStrings")
+  public void parse(String text, String expected) {
+    Assertions.assertEquals(expected, LegacyComponentSerializer.legacySection()
+        .serialize(new TimeDownParser().parse2Component(text)));
+  }
 
 }
