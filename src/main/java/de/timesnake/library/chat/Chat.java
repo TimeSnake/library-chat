bed76dd0e45fa32208e4a535f21277bccc5b9832
funzter <cavehive@protonmail.com>
2024-01-04 01:01:23 +0100

    restructured modules

---
+++ src/main/java/de/timesnake/library/chat/Chat.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2023 timesnake
+ */
+
+package de.timesnake.library.chat;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface Chat {
+
+  Component SPLITTER = Component.text("» ", ExTextColor.DARK_GRAY);
+  Component COLORED_SPLITTER = Component.text(ChatColor.DARK_PURPLE + "» ");
+
+  Component COLORED_OUT = Component.text("<- ", ExTextColor.DARK_PURPLE);
+  Component COLORED_IN = Component.text("-> ", ExTextColor.DARK_PURPLE);
+  Component COLORED_OUT_IN = Component.text("<-> ", ExTextColor.DARK_PURPLE);
+
+  static @NotNull Component getSplitter() {
+    return Component.text("» ", NamedTextColor.DARK_GRAY);
+  }
+
+  static Component getOtherSplitter() {
+    return Component.text("» ", NamedTextColor.DARK_PURPLE);
+  }
+
+  static Component getLineSeparator() {
+    return Component.text("----------", NamedTextColor.WHITE);
+  }
+
+  static String getLineTDSeparator() {
+    return "----------";
+  }
+
+  static Component getLongLineSeparator() {
+    return Component.text("---------------", NamedTextColor.WHITE);
+  }
+
+  static String getLongLineTDSeparator() {
+    return "---------------";
+  }
+
+  static Component getDoubleLineSeparator() {
+    return Component.text("==========", NamedTextColor.WHITE);
+  }
+
+  static Component getSenderPlugin(Plugin plugin) {
+    return Component.text(plugin.getName(), NamedTextColor.DARK_AQUA).append(getSplitter());
+  }
+
+  @Deprecated
+  static Component getMessageCode(String codeType, int code, Plugin plugin) {
+    return Component.text("(Code: " + codeType + code + " " + plugin.getCode() + ")");
+  }
+
+  static Component getMessageCode(Code code, ExTextColor color) {
+    return Component.text("(Code: " + code.asStringCode() + ")", color);
+  }
+
+  static Component listToComponent(Collection<?> list) {
+    if (list.isEmpty()) {
+      return Component.empty();
+    }
+
+    TextComponent.Builder builder = Component.text();
+
+    Iterator<?> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Object obj = iterator.next();
+      if (obj instanceof Component) {
+        builder.append(((Component) obj));
+      } else {
+        builder.append(Component.text(obj.toString()));
+      }
+
+      if (iterator.hasNext()) {
+        builder.append(Component.text(", "));
+      }
+    }
+
+    return builder.build();
+  }
+
+  static Component listToComponent(Collection<?> list, TextColor valueColor,
+                                   TextColor separaterColor) {
+    if (list.isEmpty()) {
+      return Component.empty();
+    }
+
+    TextComponent.Builder builder = Component.text();
+
+    Iterator<?> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Object obj = iterator.next();
+      if (obj instanceof Component) {
+        builder.append(((Component) obj).color(valueColor));
+      } else {
+        builder.append(Component.text(obj.toString(), valueColor));
+      }
+      if (iterator.hasNext()) {
+        builder.append(Component.text(", ", separaterColor));
+      }
+    }
+
+    return builder.build();
+  }
+
+  static float roundCoinAmount(float coins) {
+    return ((int) (coins * 100)) / 100f;
+  }
+
+  @Deprecated
+  static String getSplitterString() {
+    return ChatColor.DARK_GRAY + "» " + ChatColor.RESET;
+  }
+
+  @Deprecated
+  static String getOtherSplitterString() {
+    return ChatColor.DARK_PURPLE + "» " + ChatColor.RESET;
+  }
+
+  @Deprecated
+  static String getLineSeparatorString() {
+    return ChatColor.WHITE + "----------";
+  }
+
+  @Deprecated
+  static String getLongLineSeparatorString() {
+    return ChatColor.WHITE + "---------------";
+  }
+
+  @Deprecated
+  static String getDoubleLineSeparatorString() {
+    return ChatColor.WHITE + "==========";
+  }
+
+  @Deprecated
+  static String getSenderPluginString(Plugin plugin) {
+    return ChatColor.DARK_AQUA + plugin.getName() + getSplitterString();
+  }
+
+  @Deprecated
+  static String getMessageCodeString(String codeType, int code, Plugin plugin) {
+    return "(Code: " + codeType + code + " " + plugin.getCode() + ")";
+  }
+
+  static String getTimeString(Integer timeInSec) {
+    return getTimeString(Duration.ofSeconds(timeInSec));
+  }
+
+  static String getTimeString(Duration time) {
+    StringBuilder sb = new StringBuilder();
+    if (time.toDays() > 0) {
+      sb.append(time.toDays()).append("d ");
+    }
+    if (time.toHoursPart() > 0) {
+      sb.append(time.toHoursPart()).append("h ");
+    }
+    if (time.toMinutesPart() > 0) {
+      sb.append(time.toMinutesPart()).append("min ");
+    }
+    if (time.toSecondsPart() > 0) {
+      sb.append(time.toSecondsPart()).append("s");
+    }
+
+    if (sb.isEmpty()) {
+      sb.append("0s");
+    }
+
+    return sb.toString();
+  }
+
+  static String getLocationString(int x, int y, int z) {
+    return x + " " + y + " " + z;
+  }
+
+}
---
+++ src/main/java/de/timesnake/library/chat/Plugin.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2023 timesnake
+ */
+
+package de.timesnake.library.chat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Plugin {
+
+  /**
+   * @deprecated in favour of {@link #SERVER}
+   */
+  @Deprecated
+  public static final Plugin SYSTEM = new Plugin("System", "LES");
+  public static final Plugin SERVER = new Plugin("Server", "LEB");
+  public static final Plugin NETWORK = new Plugin("Network", "LEN");
+
+  public static final Plugin INFO = new Plugin("Info", "LEI");
+  public static final Plugin TIME_COINS = new Plugin("Coins", "LET");
+
+  public static final Plugin PRIVATE_MESSAGES = new Plugin("Msg", "LEM");
+  public static final Plugin MAILS = new Plugin("Mails", "LEN");
+
+  private final String name;
+  private final String code;
+
+  private final Map<String, Code> codeByString = new HashMap<>();
+
+  protected Plugin(String name, String code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getCode() {
+    return this.code;
+  }
+
+  public Code createHelpCode(String description) {
+    Code code = new Code.Builder().setPlugin(this).setType(Code.Type.HELP).setDescription(description)
+        .build();
+    this.codeByString.put(code.asStringCode(), code);
+    return code;
+  }
+
+  public Code createPermssionCode(String permission) {
+    Code code = new Code.Builder().setPlugin(this).setType(Code.Type.PERMISSION).setPermission(permission)
+        .build();
+    this.codeByString.put(code.asStringCode(), code);
+    return code;
+  }
+
+  public Code createPermssionCode(String permission, String description) {
+    Code code = new Code.Builder().setPlugin(this).setType(Code.Type.PERMISSION).setPermission(permission)
+        .setDescription(description).build();
+    this.codeByString.put(code.asStringCode(), code);
+    return code;
+  }
+
+  public Code addCode(Code code) {
+    this.codeByString.put(code.asStringCode(), code);
+    return code;
+  }
+
+  public Map<String, Code> getCodeByString() {
+    return codeByString;
+  }
+}
